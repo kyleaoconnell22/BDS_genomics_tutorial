c9b7724c03cc87ed4b86b870a2442cbfc030bb66
Kyle O'Connell <kyleaoconnell22@gmail.com>
2021-06-03 17:15:21 -0400

    Add files via upload

--- scripts/setup_tutorial.sh
+++ scripts/setup_tutorial.sh
@@ -1,15 +1,15 @@
 mkdir resources reference input results input/HG00096 results/HG00096 output_examples
-mv input.fastq.zip input/HG0096 | unzip input/HG00096/input.fastq.zip
+mv input.fastq.zip input/HG00096 | unzip input/HG00096/input.fastq.zip
 
 #Download the indexed reference genome file
 curl https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.dict -o reference/Homo_sapiens_assembly38.dict
 curl https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta -o reference/Homo_sapiens_assembly38.fasta
-curl https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta.alt -o reference/Homo_sapiens_assembly38.fasta.alt
-curl https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta.amb -o reference/Homo_sapiens_assembly38.fasta.amb
-curl https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta.ann -o reference/Homo_sapiens_assembly38.fasta.ann
-curl https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta.bwt -o reference/Homo_sapiens_assembly38.fasta.bwt
-curl https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta.pac -o reference/Homo_sapiens_assembly38.fasta.pac
-curl https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta.sa -o reference/Homo_sapiens_assembly38.fasta.sa
+curl https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta.64.alt -o reference/Homo_sapiens_assembly38.fasta.64.alt
+curl https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta.64.amb -o reference/Homo_sapiens_assembly38.fasta.64.amb
+curl https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta.64.ann -o reference/Homo_sapiens_assembly38.fasta.64.ann
+curl https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta.64.bwt -o reference/Homo_sapiens_assembly38.fasta.64.bwt
+curl https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta.64.pac -o reference/Homo_sapiens_assembly38.fasta.64.pac
+curl https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta.64.sa -o reference/Homo_sapiens_assembly38.fasta.64.sa
 curl https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta.fai -o reference/Homo_sapiens_assembly38.fasta.fai
 
 #Download the GATK Resources
